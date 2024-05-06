80b955be8c1bb619c3907d6139d2673f0577e003
unknown <Paulus Sesa>
2024-05-06 05:05:31 -0700

    avel

---
+++ edit.php
@@ -0,0 +1,76 @@
+<?php
+    // session start
+    if(!empty($_SESSION)){ }else{ session_start(); }
+    //session
+	if(!empty($_SESSION['ADMIN'])){ }else{ header('location:login.php'); }
+    // panggil file
+    require 'proses/panggil.php';
+    
+    // tampilkan form edit
+    $idGet = strip_tags($_GET['id']);
+    $hasil = $proses->tampil_data_id('tbl_user','id_login',$idGet);
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Edit Pengguna</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+    <body style="background:#586df5;">
+		<div class="container">
+			<br/>
+            <span style="color:#fff";>Selamat Datang, <?php echo $sesi['nama_pengguna'];?></span>
+			<div class="float-right">	
+				<a href="index.php" class="btn btn-success btn-md" style="margin-right:1pc;"><span class="fa fa-home"></span> Kembali</a> 
+				<a href="logout.php" class="btn btn-danger btn-md float-right"><span class="fa fa-sign-out"></span> Logout</a>
+			</div>		
+			<br/><br/><br/>
+			<div class="row">
+				<div class="col-sm-3"></div>
+				<div class="col-lg-6">
+					<br/>
+					<div class="card">
+						<div class="card-header">
+						<h4 class="card-title">Edit Pengguna - <?php echo $hasil['nama_pengguna'];?></h4>
+						</div>
+						<div class="card-body">
+						<!-- form berfungsi mengirimkan data input 
+						dengan method post ke proses crud dengan paramater get aksi edit -->
+							<form action="proses/crud.php?aksi=edit" method="POST">
+								<div class="form-group">
+									<label>Nama </label>
+									<input type="text" value="<?php echo $hasil['nama_pengguna'];?>" class="form-control" name="nama" required>
+								</div>
+								<div class="form-group">
+									<label>Telepon</label>
+									<input type="number" value="<?php echo $hasil['telepon'];?>" class="form-control" name="telepon" required>
+								</div>
+								<div class="form-group">
+									<label>Email</label>
+									<input type="harga" value="<?php echo $hasil['email'];?>" class="form-control" name="email" required>
+								</div>
+								<div class="form-group">
+									<label>Alamat</label>
+									<textarea name="alamat" class="form-control" required><?php echo $hasil['alamat'];?></textarea>
+								</div>
+								<div class="form-group">
+									<label>Username</label>
+									<input type="text" value="<?php echo $hasil['username'];?>" class="form-control" name="user" required>
+								</div>
+								<div class="form-group">
+									<label>Password</label>
+									<input type="password" value="" placeholder="ubah password" class="form-control" name="pass" required>
+									<input type="hidden" value="<?php echo $hasil['id_login'];?>" class="form-control" name="id_login" required>
+								</div>
+								<button class="btn btn-primary btn-md" name="create"><i class="fa fa-edit"> </i> Edit Data</button>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-3"></div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ tambah.php
@@ -0,0 +1,71 @@
+<?php
+    // session start
+    if(!empty($_SESSION)){ }else{ session_start(); }
+    //session
+	if(!empty($_SESSION['ADMIN'])){ }else{ header('location:login.php'); }
+    // panggil file
+    require 'proses/panggil.php';
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Tambah Pengguna</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+    <body style="background:#586df5;">
+		<div class="container">
+			<br/>
+            <span style="color:#fff";>Selamat Datang, <?php echo $sesi['nama_pengguna'];?></span>
+			<div class="float-right">	
+				<a href="index.php" class="btn btn-success btn-md" style="margin-right:1pc;"><span class="fa fa-home"></span> Kembali</a> 
+				<a href="logout.php" class="btn btn-danger btn-md float-right"><span class="fa fa-sign-out"></span> Logout</a>
+			</div>		
+			<br/><br/><br/>
+			<div class="row">
+				<div class="col-sm-3"></div>
+				<div class="col-lg-6">
+					<br/>
+					<div class="card">
+						<div class="card-header">
+						<h4 class="card-title">Tambah Pengguna</h4>
+						</div>
+						<div class="card-body">
+						<!-- form berfungsi mengirimkan data input 
+						dengan method post ke proses crud dengan paramater get aksi tambah -->
+							<form action="proses/crud.php?aksi=tambah" method="POST">
+								<div class="form-group">
+									<label>Nama </label>
+									<input type="text" value="" class="form-control" name="nama" required>
+								</div>
+								<div class="form-group">
+									<label>Telepon</label>
+									<input type="number" value="" class="form-control" name="telepon" required>
+								</div>
+								<div class="form-group">
+									<label>Email</label>
+									<input type="harga" value="" class="form-control" name="email" required>
+								</div>
+								<div class="form-group">
+									<label>Alamat</label>
+									<textarea name="alamat" class="form-control" required></textarea>
+								</div>
+								<div class="form-group">
+									<label>Username</label>
+									<input type="text" value="" class="form-control" name="user" required>
+								</div>
+								<div class="form-group">
+									<label>Password</label>
+									<input type="password" value="" class="form-control" name="pass" required>
+								</div>
+								<button class="btn btn-primary btn-md" name="create"><i class="fa fa-plus"> </i> Tambah Data</button>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-3"></div>
+			</div>
+		</div>
+	</body>
+</html>
